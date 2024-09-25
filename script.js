88556efa94f20e08131f4e006c3b1da4bb0faf29
Isabella08011 <isabellaoliveirabatista1201@gmail.com>
2024-09-25 16:55:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function solicitardados() {
+
+    //Solicita o nome do usuario
+    let nome = prompt("qual é seu nome?");
+
+    //Solicita a idade do usuario
+    let idade = prompt ("qual é a sua idade");
+
+    //exibe a saudação
+    alert ("Ola, " + nome + " você tem " + idade + " anos ");
+
+    console.log("Nome do usuário:",none);
+    console.log("Idade do usuario:",idade);
+
+    
+}
